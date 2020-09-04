754aedb9d63549f41d219062f5d10c6693bd7b00
ShivaniPR <shivani.pr@surya-soft.com>
2020-09-04 15:23:01 +0530

    add code for simple calculator

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,2 +1,25 @@
-class main {
+import java.lang.IllegalArgumentException
+
+fun main(){
+    println("Enter an arithmetic problem")
+    var input: String? = readLine()
+    while (input != null && input.isNotBlank()) {
+        var values = input.split(' ')
+        if(values.size < 3){
+            println("Invalid Input.")
+        }else{
+            val value1 = values[0].toDoubleOrNull() ?: throw IllegalArgumentException("Invalid Input")
+            val value2 = values[2].toDoubleOrNull() ?: throw IllegalArgumentException("Invalid Input")
+            val operator = values[1]
+            val result = when(operator){
+                "+" -> value1 + value2
+                "-" -> value1 - value2
+                "*" -> value1 * value2
+                "/" -> value1 / value2
+                else -> throw IllegalArgumentException("Invalid Operator $operator")
+            }
+            println("Result = $result " )
+        }
+        input = readLine()
+    }
 }
